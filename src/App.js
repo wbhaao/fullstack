a80ccd619fe4b956d8c9ac4b8ed52e66e369bb2e
wbhaao <a31620504@gmail.com>
2023-06-11 17:17:37 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,39 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // 이건 컴포넌트
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">WEB</a>
+        <a href="/">{props.title}</a>
       </h1>
     </header>
   );
 }
-
-function Nav() {
+// props로 각 태그의 값을 다르게 줄 수 있따
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.title}>
+        <a href={"/read/" + t.id}>{t.title}</a>
+      </li>
+    );
+  }
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
+      <ol>{lis}</ol>
     </nav>
   );
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: "안녕", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "js", body: "js is ..." },
+  ];
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="asd"></Header>
+      <Nav topics={topics}></Nav>
       <article>
         <h2>Welcome</h2>
         Hello, WEB
