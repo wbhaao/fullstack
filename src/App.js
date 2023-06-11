33f68f5972127e6c5e5142ec8693a09fd321e0ca
wbhaao <a31620504@gmail.com>
2023-06-11 17:06:04 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// 이건 컴포넌트
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
 function App() {
-  return <div className="App">hello react</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
+    </div>
+  );
 }
 
 export default App;
