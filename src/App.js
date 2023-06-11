9ec5ea1691bbd1757e8af4a5672eb9f034514eb3
wbhaao <a31620504@gmail.com>
2023-06-11 19:34:03 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,64 +1,98 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
-// 이건 컴포넌트
-function Header(props) {
-  return (
-    <header>
-      <h1>
-        <a href="/">{props.title}</a>
-      </h1>
-    </header>
-  );
-}
-// props로 각 태그의 값을 다르게 줄 수 있따
-function Nav(props) {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(
-      <li key={t.title}>
-        <a
-          id={t.id}
-          href={"/read/" + t.id}
-          onClick={(event) => {
-            event.preventDefault();
-            props.onC(event.target.id);
-          }}
-        >
-          {t.title}
-        </a>
-      </li>
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
     );
-  }
-  return (
-    <nav>
-      <ol>{lis}</ol>
-    </nav>
-  );
 }
-
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a
+                    href="/"
+                    onClick={(event) => {
+                        event.preventDefault();
+                        props.onChangeMode();
+                    }}
+                >
+                    {props.title}
+                </a>
+            </h1>
+        </header>
+    );
+}
+function Nav(props) {
+    const lis = [];
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a
+                    id={t.id}
+                    href={"/read/" + t.id}
+                    onClick={(event) => {
+                        event.preventDefault();
+                        props.onChangeMode(Number(event.target.id));
+                    }}
+                >
+                    {t.title}
+                </a>
+            </li>
+        );
+    }
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
+}
 function App() {
-  const topics = [
-    { id: 1, title: "안녕", body: "html is ..." },
-    { id: 2, title: "css", body: "css is ..." },
-    { id: 3, title: "js", body: "js is ..." },
-  ];
-  return (
-    <div className="App">
-      <Header title="asd"></Header>
-      <Nav
-        topics={topics}
-        onC={(id) => {
-          alert(id);
-        }}
-      ></Nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
-    </div>
-  );
+    const [mode, setMode] = useState("WELCOME");
+    const [id, setId] = useState(null);
+    const topics = [
+        { id: 1, title: "html", body: "html is ..." },
+        { id: 2, title: "css", body: "css is ..." },
+        { id: 3, title: "javascript", body: "javascript is ..." },
+    ];
+    let content = null;
+    if (mode === "WELCOME") {
+        content = <Article title="Welcome" body="Hello, WEB"></Article>;
+    } else if (mode === "READ") {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            console.log(topics[i].id, id);
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title={title} body={body}></Article>;
+    }
+    return (
+        <div>
+            <Header
+                title="WEB"
+                onChangeMode={() => {
+                    setMode("WELCOME");
+                }}
+            ></Header>
+            <Nav
+                topics={topics}
+                onChangeMode={(_id) => {
+                    setMode("READ");
+                    setId(_id);
+                }}
+            ></Nav>
+            {content}
+        </div>
+    );
 }
 
 export default App;
